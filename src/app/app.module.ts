77342b1097a3a4a5c4e3fc87e0af840bc094cb93
Vikas Vardhan <kunku.vardhan@alogent.com>
2019-03-19 15:13:36 +0530

    Angular Material installation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from "@angular/core";
 
 import { AppRoutingModule } from "./app-routing.module";
@@ -8,10 +9,20 @@ import { FormsModule } from "@angular/forms";
 
 //Third Party Modules
 import { CKEditorModule } from "@ckeditor/ckeditor5-angular";
+import { MatGridListModule } from "@angular/material/grid-list";
+import { MatButtonModule } from "@angular/material/button";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, CKEditorModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    FormsModule,
+    CKEditorModule,
+    MatGridListModule,
+    MatButtonModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
