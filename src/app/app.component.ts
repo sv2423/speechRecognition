2c553a0dc4d5f5915fb4bf6648fd0e0310a808e1
Vikas Vardhan <kunku.vardhan@alogent.com>
2019-03-27 11:11:06 +0530

    Added text to speech support

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -57,6 +57,38 @@ export class AppComponent implements OnInit {
     });
   }
 
+  public textToSpeech() {
+    var SpeechSynthesisUtterance = (window as any).SpeechSynthesisUtterance;
+    var msg = new SpeechSynthesisUtterance();
+    var voices = window.speechSynthesis.getVoices();
+    msg.voice = voices[9]; // Note: some voices don't support altering params
+    msg.voiceURI = "native";
+    msg.volume = 1; // 0 to 1
+    msg.rate = 1; // 0.1 to 10
+    msg.pitch = 1; //0 to 2
+
+    msg.text = this.getCkEditorText();
+    msg.lang = "hi";
+
+    msg.onend = function(e) {
+      console.log("Finished in " + e.elapsedTime + " seconds.");
+    };
+
+    speechSynthesis.speak(msg);
+  }
+
+  public getCkEditorText() {
+    var editor = new ClassicEditor();
+    //console.log(this.Editor.getData());
+    console.log(editor.getSnapshot());
+    var html = editor.getSnapshot();
+    var dom = document.createElement("DIV");
+    dom.innerHTML = html;
+    var plain_text = dom.textContent || dom.innerText;
+
+    return plain_text;
+  }
+
   /**
    *
    */
